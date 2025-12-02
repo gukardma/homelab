ee2ce4346b7cba54501de52d30bbcbbd8e6740b7
gukardma <gukardm@gmail.com>
2025-12-01 22:46:01 -0500

    sanitize

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -5,18 +5,14 @@ locals {
 module "vm" {
   source = "./modules/proxmox_vm"
 
-  target_node = "pve01"
-  clone       = "ubuntu-cloudinit"
+  target_node = var.target_node
+  clone       = var.clone_template
   count       = length(local.vm_names)
   vmid        = 300 + count.index
   name        = local.vm_names[count.index]
 
-  ci_user         = "ubuntu"
+  ci_user         = var.ci_user
   ci_ipv4_cidr    = "192.168.20.10${count.index}/24"
-  ci_ipv4_gateway = "192.168.20.1"
-  ci_ssh_key      = <<EOF
-                    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtLipDkji+y8oQlbUy1Ai0Wf0xa2wRXaAW0ZLmmbjo9 guichard@gumani
-                    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPi3OwaJ8oztZ7cWtldji1bc06d5DSit8L47x/ge79fc ubuntu@gumani
-                    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVoklkY0ze+c2vAL8xp7fPeeujbOilYOnt8eZPmkNdO guichard@xps
-                    EOF
+  ci_ipv4_gateway = var.ci_ipv4_gateway
+  ci_ssh_key      = var.ci_ssh_keys
 }
